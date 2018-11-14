e3c5082eaf6061e12bc202d17c77a885d954036d
LeonardoSarallo <leo.sarallo@gmail.com>
2018-11-14 15:23:34 +0100

    nocheck

--- main.js
+++ main.js
@@ -3,9 +3,9 @@ var mailutente = prompt('Inserisci la tua mail');
 // creo una lista di mail con array
 var listamail = ['pippo@pluto.com', 'topolino@pluto.com, paperino@pluto.com'];
 // elaboro la risposta
+
 if (mailutente == listamail) {
   document.getElementById('answer').innerHTML = 'Login effettuato con successo';
-
 }
 else {
   document.getElementById('answer').innerHTML = 'Login fallito';
